e0c391da75d92b5c1282103e25c96ae6e4afd9c5
David Weng <dkweng@gmail.com>
2017-04-12 09:58:37 -0400

    Copied Map and River

--- src/com/iteration3/model/Map/Map.java
+++ src/com/iteration3/model/Map/Map.java
@@ -1,4 +1,80 @@
 package com.iteration3.model.Map;
 
+import java.util.HashMap;
+
+import com.iteration3.model.Tiles.Tile;
+import com.iteration3.model.Visitors.TerrainTypeVisitor;
+
+
 public class Map {
+
+    private HashMap<Location, Tile> tiles;
+    private HashMap<Location, River> rivers;
+
+    public Map() {
+        tiles = new HashMap<>();
+        rivers = new HashMap<>();
+    }
+
+    public void addTileFromFile(Location l, Tile t) {
+        if(validateLocationRange(l)) {
+            tiles.put(l,t);
+        }
+    }
+
+    public void addRiverFromFile(Location l, River r) {
+        if(validateLocationRange(l)) {
+            rivers.put(l, r);
+        }
+    }
+
+    public void clearMap() {
+        rivers.clear();
+        tiles.clear();
+    }
+
+
+    public boolean validateLocationRange(Location location) {
+        if(location.getX() > Math.abs(10) || location.getY() > Math.abs(10) || location.getZ() > Math.abs(10)) {
+            System.out.println("Invalid location type");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean isSeaTile(Location l) {
+        if(tiles.get(l).getTerrain(new TerrainTypeVisitor()).equals("sea")) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public HashMap<Location, Tile> getTiles() {
+        return tiles;
+    }
+
+    public HashMap<Location, River> getRivers() {
+        return rivers;
+    }
+
+    public void printRivers() {
+        for(Location location : rivers.keySet()) {
+            System.out.println(rivers.get(location) + " " + Integer.toString(location.getX()) + " " +  Integer.toString(location.getY()) + " " + Integer.toString(location.getZ()));
+            rivers.get(location).printRiverEdges();
+        }
+    }
+
+    public void printTiles() {
+        for(Location location : tiles.keySet()) {
+            System.out.println(tiles.get(location) + " " + Integer.toString(location.getX()) + " " +  Integer.toString(location.getY()) + " " + Integer.toString(location.getZ()));
+            System.out.println("Terrain: " + tiles.get(location).getTerrain(new TerrainTypeVisitor()));
+        }
+    }
+
+
+
+
 }
+
--- src/com/iteration3/model/Map/River.java
+++ src/com/iteration3/model/Map/River.java
@@ -1,4 +1,114 @@
 package com.iteration3.model.Map;
 
+import java.util.ArrayList;
+
 public class River {
+
+    // 1 is North, 2 is NorthEast, 3 is SouthEast, 4 is South, 5 is SouthWest, 6 is NorthWest
+    private ArrayList<Integer> riverEdges;
+
+    public River() {
+        this.riverEdges = new ArrayList<Integer>();
+    }
+
+    public River(ArrayList<Integer> arrayList) { this.riverEdges = arrayList; }
+
+    // Constructors for river types with various number of edges
+    public River(int edge1) {
+        this.riverEdges = new ArrayList<Integer>();
+        this.riverEdges.add(edge1);
+    }
+
+    public River(int edge1, int edge2) {
+        this.riverEdges = new ArrayList<Integer>();
+        this.riverEdges.add(edge1);
+        this.riverEdges.add(edge2);
+    }
+
+    public River(int edge1, int edge2, int edge3) {
+        this.riverEdges = new ArrayList<Integer>();
+        this.riverEdges.add(edge1);
+        this.riverEdges.add(edge2);
+        this.riverEdges.add(edge3);
+    }
+
+    // add edge if that edge isn't already in the list and if size < 3
+    public void addRiverEdge(int edge) {
+        if(this.riverEdges.size() < 3 && !riverEdges.contains(Integer.valueOf(edge))) {
+            this.riverEdges.add(edge);
+        }
+    }
+
+    public void removeRiverEdge(int edge) {
+        this.riverEdges.remove(Integer.valueOf(edge));
+    }
+
+    public ArrayList<Integer> getRiverEdges() {
+        return riverEdges;
+    }
+
+    public void setRiverEdges(ArrayList<Integer> riverEdges) {
+        this.riverEdges = riverEdges;
+    }
+
+    public void rotateCounterClockwise() {
+        for(int i = 0; i < riverEdges.size(); i++) {
+            if(riverEdges.get(i) == 1) {
+                riverEdges.set(i, 6);
+            }
+            else if(riverEdges.get(i) == 6) {
+                riverEdges.set(i, 5);
+            }
+            else if(riverEdges.get(i) == 5) {
+                riverEdges.set(i, 4);
+            }
+            else if(riverEdges.get(i) == 4) {
+                riverEdges.set(i, 3);
+            }
+            else if(riverEdges.get(i) == 3) {
+                riverEdges.set(i, 2);
+            }
+            else if(riverEdges.get(i) == 2) {
+                riverEdges.set(i, 1);
+            }
+        }
+    }
+
+    public void rotateClockwise() {
+        for(int i = 0; i < riverEdges.size(); i++) {
+            if(riverEdges.get(i) == 1) {
+                riverEdges.set(i, 2);
+            }
+            else if(riverEdges.get(i) == 2) {
+                riverEdges.set(i, 3);
+            }
+            else if(riverEdges.get(i) == 3) {
+                riverEdges.set(i, 4);
+            }
+            else if(riverEdges.get(i) == 4) {
+                riverEdges.set(i, 5);
+            }
+            else if(riverEdges.get(i) == 5) {
+                riverEdges.set(i, 6);
+            }
+            else if(riverEdges.get(i) == 6) {
+                riverEdges.set(i, 1);
+            }
+        }
+    }
+
+    public boolean containsEdge(int i){
+        if(riverEdges.contains(i)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void printRiverEdges() {
+        System.out.println("Edges: " + riverEdges);
+    }
+
 }
+
