cfeb1a0c5bd537be44d4bbdd3f09d7cd03190e1d
acoverstone <acoverstone@ufl.edu>
2017-04-12 19:16:15 -0400

    Added bridges to rivers and created test

--- src/com/iteration3/model/Map/River.java
+++ src/com/iteration3/model/Map/River.java
@@ -6,31 +6,41 @@ public class River {
 
     // 1 is North, 2 is NorthEast, 3 is SouthEast, 4 is South, 5 is SouthWest, 6 is NorthWest
     private ArrayList<Integer> riverEdges;
+    private ArrayList<Integer> bridges;
 
     public River() {
         this.riverEdges = new ArrayList<Integer>();
+        this.bridges = new ArrayList<Integer>();
     }
 
-    public River(ArrayList<Integer> arrayList) { this.riverEdges = arrayList; }
+    public River(ArrayList<Integer> arrayList) {
+        this.riverEdges = arrayList;
+        this.bridges = new ArrayList<Integer>();
+    }
 
     // Constructors for river types with various number of edges
     public River(int edge1) {
         this.riverEdges = new ArrayList<Integer>();
+        this.bridges = new ArrayList<Integer>();
         this.riverEdges.add(edge1);
     }
 
     public River(int edge1, int edge2) {
         this.riverEdges = new ArrayList<Integer>();
+        this.bridges = new ArrayList<Integer>();
         this.riverEdges.add(edge1);
         this.riverEdges.add(edge2);
     }
 
     public River(int edge1, int edge2, int edge3) {
         this.riverEdges = new ArrayList<Integer>();
+        this.bridges = new ArrayList<Integer>();
         this.riverEdges.add(edge1);
         this.riverEdges.add(edge2);
         this.riverEdges.add(edge3);
     }
+
+    // riveredge helper functions
 
     // add edge if that edge isn't already in the list and if size < 3
     public void addRiverEdge(int edge) {
@@ -51,53 +61,8 @@ public class River {
         this.riverEdges = riverEdges;
     }
 
-    public void rotateCounterClockwise() {
-        for(int i = 0; i < riverEdges.size(); i++) {
-            if(riverEdges.get(i) == 1) {
-                riverEdges.set(i, 6);
-            }
-            else if(riverEdges.get(i) == 6) {
-                riverEdges.set(i, 5);
-            }
-            else if(riverEdges.get(i) == 5) {
-                riverEdges.set(i, 4);
-            }
-            else if(riverEdges.get(i) == 4) {
-                riverEdges.set(i, 3);
-            }
-            else if(riverEdges.get(i) == 3) {
-                riverEdges.set(i, 2);
-            }
-            else if(riverEdges.get(i) == 2) {
-                riverEdges.set(i, 1);
-            }
-        }
-    }
 
-    public void rotateClockwise() {
-        for(int i = 0; i < riverEdges.size(); i++) {
-            if(riverEdges.get(i) == 1) {
-                riverEdges.set(i, 2);
-            }
-            else if(riverEdges.get(i) == 2) {
-                riverEdges.set(i, 3);
-            }
-            else if(riverEdges.get(i) == 3) {
-                riverEdges.set(i, 4);
-            }
-            else if(riverEdges.get(i) == 4) {
-                riverEdges.set(i, 5);
-            }
-            else if(riverEdges.get(i) == 5) {
-                riverEdges.set(i, 6);
-            }
-            else if(riverEdges.get(i) == 6) {
-                riverEdges.set(i, 1);
-            }
-        }
-    }
-
-    public boolean containsEdge(int i){
+    public boolean containsRiverEdge(int i){
         if(riverEdges.contains(i)){
             return true;
         }
@@ -106,8 +71,40 @@ public class River {
         }
     }
 
-    public void printRiverEdges() {
-        System.out.println("Edges: " + riverEdges);
+    public void printRiverEdgesandBridges() {
+        System.out.println("Edges: " + this.riverEdges);
+        System.out.println("Bridges:" + this.bridges);
+    }
+
+
+
+    // bridge helper functions
+    // add bridge if that bridge isn't already in the list and if size < 3 and there is river there
+    public void addBridge(int edge) {
+        if(this.bridges.size() < 3 && !this.bridges.contains(Integer.valueOf(edge)) && this.riverEdges.contains(Integer.valueOf(edge))) {
+            this.bridges.add(edge);
+        }
+    }
+
+    public void removeBridge(int edge) {
+        this.bridges.remove(Integer.valueOf(edge));
+    }
+
+    public ArrayList<Integer> getBridges() {
+        return bridges;
+    }
+
+    public void setBridges(ArrayList<Integer> bridges) {
+        this.bridges = bridges;
+    }
+
+    public boolean containsBridge(int i){
+        if(bridges.contains(i)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 }
