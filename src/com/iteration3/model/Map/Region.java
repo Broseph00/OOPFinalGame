7dda6d5b0125c21ea89258e0f8454d87b3013ca8
David Weng <dkweng@gmail.com>
2017-04-13 17:14:27 -0400

    Start Region

--- src/com/iteration3/model/Map/Region.java
+++ src/com/iteration3/model/Map/Region.java
@@ -1,4 +1,26 @@
 package com.iteration3.model.Map;
 
+import java.util.ArrayList;
+
 public class Region {
+    private ArrayList<ArrayList<Integer>> regions;
+    public Region(){
+        regions = new ArrayList<>();
+        ArrayList<Integer> init = new ArrayList<>();
+        init.add(1);
+        init.add(2);
+        init.add(3);
+        init.add(4);
+        init.add(5);
+        init.add(6);
+        regions.add(init);
+    }
+
+    public void addRiver(River river){
+        
+    }
+
+    public void addBridge(Integer edge){
+
+    }
 }
