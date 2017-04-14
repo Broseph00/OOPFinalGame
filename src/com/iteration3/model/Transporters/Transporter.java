392357916865aab92dc25e77e0c9feaabac01e0f
oliverluotom <oliverluotom@ufl.edu>
2017-04-14 17:53:09 -0400

    merging

--- src/com/iteration3/model/Transporters/Transporter.java
+++ src/com/iteration3/model/Transporters/Transporter.java
@@ -1,4 +1,12 @@
 package com.iteration3.model.Transporters;
 
+import com.iteration3.model.Resource.Resource;
+
+import java.util.List;
+
 public abstract class Transporter {
+    private List<Resource> resourceList;
+    private int remainingMovePoints;
+    private int maxMovePoints;
+    private int capacity;
 }
