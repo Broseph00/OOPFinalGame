e43653b6b940705f323c82a3a50d8bbb492da8a9
acoverstone <acoverstone@ufl.edu>
2017-04-15 13:41:42 -0400

    Fixed secondary producers

--- src/com/iteration3/model/Buildings/Secondary/StoneFactory.java
+++ src/com/iteration3/model/Buildings/Secondary/StoneFactory.java
@@ -1,4 +1,7 @@
 package com.iteration3.model.Buildings.Secondary;
 
 public class StoneFactory extends SecondaryProducer {
+    public StoneFactory(int maxCapacity) {
+        super(maxCapacity);
+    }
 }
