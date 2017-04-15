0fe8d3b5cb1e4fc6271089d4ce52d8f911c0f9b6
PGA <philipa@ufl.edu>
2017-04-15 14:55:24 -0400

    Corrected constructors for current inheritance

--- src/com/iteration3/model/Buildings/Transporter/RowboatFactory.java
+++ src/com/iteration3/model/Buildings/Transporter/RowboatFactory.java
@@ -1,4 +1,8 @@
 package com.iteration3.model.Buildings.Transporter;
 
 public class RowboatFactory extends TransporterFactory {
+    @Override
+    public TransporterFactory upgrade() {
+        return null;
+    }
 }
--- src/com/iteration3/model/Buildings/Transporter/SteamerFactory.java
+++ src/com/iteration3/model/Buildings/Transporter/SteamerFactory.java
@@ -1,4 +1,8 @@
 package com.iteration3.model.Buildings.Transporter;
 
 public class SteamerFactory extends TransporterFactory {
+    @Override
+    public TransporterFactory upgrade() {
+        return null;
+    }
 }
