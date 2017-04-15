eca527be537eebcf59b570a9e3974188e46483cb
PGA <philipa@ufl.edu>
2017-04-15 18:11:22 -0400

    Transporter factories now extend Producer class (previously
    ResourceProducer)

--- src/com/iteration3/model/Buildings/Transporter/RaftFactory.java
+++ src/com/iteration3/model/Buildings/Transporter/RaftFactory.java
@@ -1,6 +1,10 @@
 package com.iteration3.model.Buildings.Transporter;
 
 public class RaftFactory extends TransporterFactory {
+    public RaftFactory() {
+        super(1);
+    }
+
     @Override
     public TransporterFactory upgrade() {
         return null;
--- src/com/iteration3/model/Buildings/Transporter/TruckFactory.java
+++ src/com/iteration3/model/Buildings/Transporter/TruckFactory.java
@@ -1,6 +1,10 @@
 package com.iteration3.model.Buildings.Transporter;
 
 public class TruckFactory extends TransporterFactory {
+    public TruckFactory() {
+        super(1);
+    }
+
     @Override
     public TransporterFactory upgrade() {
         return null;
