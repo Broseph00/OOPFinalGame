345d55b3123711f1467186c2a9e4d88bd8ddd240
PGA <philipa@ufl.edu>
2017-04-15 14:51:43 -0400

    Continued implementation of primary producers

--- src/com/iteration3/model/Buildings/Primary/PrimaryProducer.java
+++ src/com/iteration3/model/Buildings/Primary/PrimaryProducer.java
@@ -2,24 +2,30 @@ package com.iteration3.model.Buildings.Primary;
 
 import com.iteration3.model.Buildings.ResourceProducer;
 import com.iteration3.model.Resource.Resource;
-import com.iteration3.model.Tiles.Terrain;
+import com.iteration3.model.Tiles.*;
 import com.iteration3.model.Visitors.TerrainTypeVisitor;
 
 public abstract class PrimaryProducer extends ResourceProducer {
     private Terrain myTerrain;
+    private TerrainTypeVisitor terrainVisitor;
 
     public PrimaryProducer(Terrain requiredTerrain){
         super(1);
         myTerrain = requiredTerrain;
+        terrainVisitor = new TerrainTypeVisitor();
     }
 
-    public final boolean verifyTerrain(Terrain terrain){
-//        String myTerrainType = myTerrain.getTerrainType(terrainVisitor);
-//        String otherTerrainType = terrain.getTerrainType(terrainVisitor);
-//        // TODO: inquire about terrain equals method
-//        return myTerrainType.matches(otherTerrainType);
-        myTerrain.getClass();
-        return false;
+    public boolean verifyLocation(Tile tile){
+        Terrain terrain = tile.getTerrain();
+        return verifyTerrain(terrain);
+    }
+
+    private boolean verifyTerrain(Terrain terrain){
+        String myTerrainType = myTerrain.getTerrainType(terrainVisitor);
+        String otherTerrainType = terrain.getTerrainType(terrainVisitor);
+        // TODO: inquire about terrain equals method
+
+        return myTerrainType.matches(otherTerrainType);
     }
 
     public abstract Resource produce();
--- src/com/iteration3/model/Buildings/Primary/Woodcutter.java
+++ src/com/iteration3/model/Buildings/Primary/Woodcutter.java
@@ -1,4 +1,20 @@
 package com.iteration3.model.Buildings.Primary;
 
+import com.iteration3.model.Resource.Resource;
+import com.iteration3.model.Tiles.Terrain;
+import com.iteration3.model.Tiles.Tile;
+import com.iteration3.model.Tiles.WoodTerrain;
+
 public class Woodcutter extends PrimaryProducer {
+    Tile location;
+
+    public Woodcutter(Tile buildLocation) {
+        super(new WoodTerrain());
+        location = buildLocation;
+    }
+
+    @Override
+    public Resource produce() {
+        return null;
+    }
 }
