45657336e7ad280c06db0040ddd79f9299216ad6
PGA <philipa@ufl.edu>
2017-04-16 18:54:45 -0400

    Secondary producers created

--- src/com/iteration3/model/Buildings/Secondary/CoalBurner.java
+++ src/com/iteration3/model/Buildings/Secondary/CoalBurner.java
@@ -1,7 +1,52 @@
 package com.iteration3.model.Buildings.Secondary;
 
+import com.iteration3.model.Resource.*;
+
+import java.util.ArrayList;
+
 public class CoalBurner extends SecondaryProducer {
-    public CoalBurner(int maxCapacity) {
-        super(maxCapacity);
+    private ArrayList<ResourceRequirement> necessaryResources;
+
+    public CoalBurner() {
+        super(6);
+        initialize();
+    }
+
+    private void initialize(){
+        ResourceRequirement req1 = new ResourceRequirement(new Trunk(), new Trunk());
+        necessaryResources.add(req1);
+
+        ResourceRequirement req2 = new ResourceRequirement(new Trunk(), new Board());
+        necessaryResources.add(req2);
+
+        ResourceRequirement req3 = new ResourceRequirement(new Board(), new Board());
+        necessaryResources.add(req3);
+    }
+
+    @Override
+    public boolean checkResources(ResourceList availableResources) {
+        for(ResourceRequirement reqs : necessaryResources){
+            if(reqs.verify(availableResources.getResources())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public ArrayList<Resource> produce(ResourceList availableResources) {
+        boolean canProduce = false;
+        ArrayList<Resource> fuels = new ArrayList<>();
+
+        for(ResourceRequirement reqs : necessaryResources){
+            canProduce = reqs.consume(availableResources.getResources());
+            if(canProduce && (getCapacity() > 0)) {
+                fuels.add(new Fuel());
+                decrementCapacity();
+                break;
+            }
+        }
+
+        return fuels;
     }
 }
--- src/com/iteration3/model/Buildings/Secondary/SecondaryProducer.java
+++ src/com/iteration3/model/Buildings/Secondary/SecondaryProducer.java
@@ -1,10 +1,17 @@
 package com.iteration3.model.Buildings.Secondary;
 
 import com.iteration3.model.Buildings.Producer;
+import com.iteration3.model.Resource.Resource;
+import com.iteration3.model.Resource.ResourceList;
+
+import java.util.ArrayList;
 
 public abstract class SecondaryProducer extends Producer {
-    public SecondaryProducer(){super(1);}
     public SecondaryProducer(int maxCapacity){
         super(maxCapacity);
     }
+
+    public abstract boolean checkResources(ResourceList availableResources);
+
+    public abstract ArrayList<Resource> produce(ResourceList availableResources);
 }
