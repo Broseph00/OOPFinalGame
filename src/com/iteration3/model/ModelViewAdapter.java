709bdd2dbd44b5298b680ee86e337f3c9b0bacf7
Leslie Richardson <bookworm42795@gmail.com>
2017-04-16 13:15:59 -0400

    Roads and Boats text file now loads in the view

--- src/com/iteration3/model/ModelViewAdapter.java
+++ src/com/iteration3/model/ModelViewAdapter.java
@@ -1,21 +1,419 @@
 package com.iteration3.model;
 
+import com.iteration3.model.Abilities.*;
+import com.iteration3.model.Managers.MapFileManager;
+import com.iteration3.model.Map.Location;
+import com.iteration3.model.Map.Map;
+import com.iteration3.model.Players.Research.*;
+import com.iteration3.model.Tiles.*;
+import com.iteration3.model.Visitors.Visitor;
 import com.iteration3.view.GameWindow;
 import com.iteration3.view.MainView;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by LesliesLaptop on 4/11/17.
  */
-public class ModelViewAdapter {
+//TODO: Consider breaking up this class into more manageable chunks?
+public class ModelViewAdapter implements Visitor {
     private GameModel gameModel;
     private GameWindow gameWindow;
-    //private Location currentLocation;
+    private Location currentLocation;
 
 
-    public ModelViewAdapter(GameModel gm, GameWindow gw){
+    public ModelViewAdapter(GameModel gm, GameWindow gw) {
         gameModel = gm;
         gameWindow = gw;
     }
 
-    public void update() {}
+    public void update() {
+        Map map = gameModel.getMap();
+        try {
+            MapFileManager fileManager = new MapFileManager(map, "src/com/iteration3/RoadsAndBoatsMap.txt");
+            fileManager.fillMapFromTextFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        map.printRivers();
+        drawTiles(map);
+        drawRivers(map);
+    }
+
+    private void drawTiles(Map map) {
+        for(Location location : map.getTiles().keySet()) {
+            currentLocation = location;
+            map.getTiles().get(location).getTerrain().acceptVisitor(this);
+        }
+
+
+    }
+
+    private void drawRivers(Map map) {
+
+        for(Location location : map.getRivers().keySet()) {
+            currentLocation = location;
+            ArrayList<Integer> riverEdges = map.getRivers().get(location).getRiverEdges();
+
+            // handle river sources
+            if(riverEdges.size() == 1) {
+                if(riverEdges.contains(1)) {
+                    gameWindow.drawRiver("source1",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(2)) {
+                    gameWindow.drawRiver("source2",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(3)) {
+                    gameWindow.drawRiver("source3",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(4)) {
+                    gameWindow.drawRiver("source4",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(5)) {
+                    gameWindow.drawRiver("source5",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(6)) {
+                    gameWindow.drawRiver("source6",  currentLocation.getX(), currentLocation.getZ());
+                }
+            }
+            else if(riverEdges.size() == 2) {
+                // handle adjacent rivers
+                if(riverEdges.contains(1) && riverEdges.contains(2)) {
+                    gameWindow.drawRiver("adj1",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(2) && riverEdges.contains(3)) {
+                    gameWindow.drawRiver("adj2",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(3) && riverEdges.contains(4)) {
+                    gameWindow.drawRiver("adj3",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(4) && riverEdges.contains(5)) {
+                    gameWindow.drawRiver("adj4",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(5) && riverEdges.contains(6)) {
+                    gameWindow.drawRiver("adj5",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(6) && riverEdges.contains(1)) {
+                    gameWindow.drawRiver("adj6",  currentLocation.getX(), currentLocation.getZ());
+                }
+                // handle angled
+                else if(riverEdges.contains(1) && riverEdges.contains(3)) {
+                    gameWindow.drawRiver("angled1",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(2) && riverEdges.contains(4)) {
+                    gameWindow.drawRiver("angled2",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(3) && riverEdges.contains(5)) {
+                    gameWindow.drawRiver("angled3",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(4) && riverEdges.contains(6)) {
+                    gameWindow.drawRiver("angled4",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(5) && riverEdges.contains(1)) {
+                    gameWindow.drawRiver("angled5",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(6) && riverEdges.contains(2)) {
+                    gameWindow.drawRiver("angled6",  currentLocation.getX(), currentLocation.getZ());
+                }
+                // handle straight
+                else if(riverEdges.contains(1) && riverEdges.contains(4)) {
+                    gameWindow.drawRiver("straight1",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(2) && riverEdges.contains(5)) {
+                    gameWindow.drawRiver("straight2",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else if(riverEdges.contains(3) && riverEdges.contains(6)) {
+                    gameWindow.drawRiver("straight3",  currentLocation.getX(), currentLocation.getZ());
+                }
+
+            }
+            // handle triple rivers
+            else if(riverEdges.size() == 3) {
+                if(riverEdges.contains(1)) {
+                    gameWindow.drawRiver("tri1",  currentLocation.getX(), currentLocation.getZ());
+                }
+                else {
+                    gameWindow.drawRiver("tri2",  currentLocation.getX(), currentLocation.getZ());
+                }
+            }
+
+        }
+
+
+    }
+
+
+    @Override
+    public void visit(WoodTerrain terrain) {
+        gameWindow.drawTile("woods", currentLocation.getX(), currentLocation.getZ());
+    }
+
+    @Override
+    public void visit(MountainTerrain terrain) {
+        gameWindow.drawTile("mountains", currentLocation.getX(), currentLocation.getZ());
+    }
+
+    @Override
+    public void visit(DesertTerrain terrain) {
+        gameWindow.drawTile("desert", currentLocation.getX(), currentLocation.getZ());
+    }
+
+    @Override
+    public void visit(PastureTerrain terrain) {
+        gameWindow.drawTile("pasture", currentLocation.getX(), currentLocation.getZ());
+
+    }
+
+    @Override
+    public void visit(RockTerrain terrain) {
+        gameWindow.drawTile("rock", currentLocation.getX(), currentLocation.getZ());
+
+    }
+
+    @Override
+    public void visit(SeaTerrain terrain) {
+        gameWindow.drawTile("sea", currentLocation.getX(), currentLocation.getZ());
+    }
+
+    @Override
+    public void visit(EnlargementResearch research) {
+
+    }
+
+    @Override
+    public void visit(NewShaftResearch research) {
+
+    }
+
+    @Override
+    public void visit(OilResearch research) {
+
+    }
+
+    @Override
+    public void visit(RowingResearch research) {
+
+    }
+
+    @Override
+    public void visit(ShipResearch research) {
+
+    }
+
+    @Override
+    public void visit(SpecializationResearch research) {
+
+    }
+
+    @Override
+    public void visit(TruckResearch research) {
+
+    }
+
+    @Override
+    public void visit(BuildClaypitAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildCoalBurnerAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildMineAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildMintAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildOilRigAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildPapermillAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildRaftFactoryAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildRowboatFactoryAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildSteamerFactoryAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildStockExchangeAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildStoneFactoryAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildStoneQuarryAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildTruckFactoryAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildWagonFactoryAbility ability) {
+
+    }
+
+    @Override
+    public void visit(BuildWoodcutterAbility ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree0Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree30Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree60Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree90Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree120Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree150Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree180Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree210Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree240Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree270Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree300Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveDegree330Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveEdge1Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveEdge2Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveEdge3Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveEdge4Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveEdge5Ability ability) {
+
+    }
+
+    @Override
+    public void visit(MoveEdge6Ability ability) {
+
+    }
+
+    @Override
+    public void visit(DockatSea1Ability ability) {
+
+    }
+
+    @Override
+    public void visit(DockatSea2Ability ability) {
+
+    }
+
+    @Override
+    public void visit(DockatSea3Ability ability) {
+
+    }
+
+    @Override
+    public void visit(DockatSea4Ability ability) {
+
+    }
+
+    @Override
+    public void visit(DockatSea5Ability ability) {
+
+    }
+
+    @Override
+    public void visit(DockatSea6Ability ability) {
+
+    }
+
+    @Override
+    public void visit(DockatRiverAbility ability) {
+
+    }
+
+    @Override
+    public void visit(UndockAbility ability) {
+
+    }
 }
