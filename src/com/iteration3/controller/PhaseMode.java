4a5c9d3fd2c71ac884d8f7a2ea03281612911328
Clay Schnars <cschnars13@ufl.edu>
2017-04-16 11:21:59 -0400

    Starting on building modes

---
+++ src/com/iteration3/controller/PhaseMode.java
@@ -0,0 +1,7 @@
+package com.iteration3.controller;
+
+/**
+ * Created by Clay on 4/16/2017.
+ */
+public interface PhaseMode {
+}
---
+++ src/com/iteration3/controller/StructureMode.java
@@ -0,0 +1,55 @@
+package com.iteration3.controller;
+
+import com.iteration3.model.GameModel;
+import com.iteration3.view.GameWindow;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Clay on 4/16/2017.
+ */
+public class StructureMode implements CycleMode {
+
+    ArrayList<Action> buildings;
+    int index;
+
+    public StructureMode(GameModel model, GameWindow window){
+        initActions();
+        index = 0;
+    }
+
+    public void initActions(){
+        buildings.add(new Action(){
+            public void execute() {
+                //model.createSawmill();
+            }
+        });
+
+        buildings.add(new Action(){
+            public void execute() {
+                //model.createSawmill();
+            }
+        });
+
+        buildings.add(new Action(){
+            public void execute() {
+                //model.createSawmill();
+            }
+        });
+    }
+
+    public void next(){
+        index++;
+        index %= buildings.size();
+    }
+
+    public void prev(){
+        index--;
+        if (index < 0)
+            index = buildings.size()-1;
+    }
+
+    public void execute(){
+        buildings.get(index).execute();
+    }
+}
