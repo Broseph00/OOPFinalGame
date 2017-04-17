4b55c8e7bf2d3c983b147da67f347884e9cefd83
PGA <philipa@ufl.edu>
2017-04-16 21:52:42 -0400

    Updated mines

--- src/com/iteration3/model/Buildings/Primary/GoldMine.java
+++ src/com/iteration3/model/Buildings/Primary/GoldMine.java
@@ -2,11 +2,10 @@ package com.iteration3.model.Buildings.Primary;
 
 import com.iteration3.model.Resource.*;
 
-public class BigMine extends Mine {
+public class GoldMine extends Mine{
     @Override
     public void buildShaft() {
-        for(int i = 0; i < 5; ++i){
-            oreBag.add(new Iron());
+        for(int i = 0; i < 4; ++i){
             oreBag.add(new Gold());
         }
     }
